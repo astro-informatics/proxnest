ce0bdc26f2cffa05e0719340b63f225bc2d4c7e0
Tobias Liaudat <tobiasliaudat@gmail.com>
2024-04-11 17:17:22 +0200

    black formatting

--- mywork/Gaussian_Benchmark/estimations.py
+++ mywork/Gaussian_Benchmark/estimations.py
@@ -5,20 +5,26 @@ import os
 
 res_dir = "PosFalse3/2024_03_07"
 
-dirs = [f for f in os.listdir(res_dir) if os.path.isdir(os.path.join(res_dir,f))]
+dirs = [f for f in os.listdir(res_dir) if os.path.isdir(os.path.join(res_dir, f))]
 os.chdir(res_dir)
 
 df = pd.DataFrame()
 n = 1
 for i in dirs:
     if df.empty:
-        df = pd.read_csv(i+"/predictions.csv", names=("dimension", "ground truth", f"predictions {n}"))
+        df = pd.read_csv(
+            i + "/predictions.csv",
+            names=("dimension", "ground truth", f"predictions {n}"),
+        )
     else:
-        preds = pd.read_csv(i+"/predictions.csv", names=("dimension", "ground truth", f"predictions {n}"))
-        df = pd.merge(df,preds)
-    n+=1
+        preds = pd.read_csv(
+            i + "/predictions.csv",
+            names=("dimension", "ground truth", f"predictions {n}"),
+        )
+        df = pd.merge(df, preds)
+    n += 1
 
-df.set_index('dimension', inplace=True)
+df.set_index("dimension", inplace=True)
 df = df.T
 df.to_csv("Predictions.csv")
 
@@ -31,11 +37,20 @@ avgs = np.mean(df[1:], axis=0)
 stds = np.std(df[1:], axis=0)
 
 plt.figure(dpi=300)
-plt.errorbar(dims, avgs, yerr=stds, marker="x", capsize=2, 
-             linewidth=1, elinewidth=0.5, color="red", ecolor="blue")
-plt.plot(dims, df[0], linewidth=1, color='black')
-plt.ylim(-10,90)
-plt.xlim(-5,210)
+plt.errorbar(
+    dims,
+    avgs,
+    yerr=stds,
+    marker="x",
+    capsize=2,
+    linewidth=1,
+    elinewidth=0.5,
+    color="red",
+    ecolor="blue",
+)
+plt.plot(dims, df[0], linewidth=1, color="black")
+plt.ylim(-10, 90)
+plt.xlim(-5, 210)
 plt.xlabel("Dimensions")
 plt.ylabel(r"$\log (V \times \mathcal{Z})$")
 plt.savefig("Avg NS Estimates")
--- mywork/Gaussian_Benchmark/residuals.py
+++ mywork/Gaussian_Benchmark/residuals.py
@@ -5,20 +5,20 @@ import os
 
 res_dir = "mywork/Gaussian Benchmark/Residuals2/2024_03_06"
 
-dirs = [f for f in os.listdir(res_dir) if os.path.isdir(os.path.join(res_dir,f))]
+dirs = [f for f in os.listdir(res_dir) if os.path.isdir(os.path.join(res_dir, f))]
 os.chdir(res_dir)
 
 df = pd.DataFrame()
 n = 1
 for i in dirs:
     if df.empty:
-        df = pd.read_csv(i+"/residuals.csv", names=("dimension", f"run {n}"))
+        df = pd.read_csv(i + "/residuals.csv", names=("dimension", f"run {n}"))
     else:
-        res = pd.read_csv(i+"/residuals.csv", names=("dimension", f"run {n}"))
-        df = pd.merge(df,res)
-    n+=1
+        res = pd.read_csv(i + "/residuals.csv", names=("dimension", f"run {n}"))
+        df = pd.merge(df, res)
+    n += 1
 
-df.set_index('dimension', inplace=True)
+df.set_index("dimension", inplace=True)
 df = df.T
 df.to_csv("dataframe.csv")
 
@@ -26,8 +26,17 @@ avgs = np.mean(df.to_numpy(), axis=0)
 stds = np.std(df.to_numpy(), axis=0)
 
 plt.figure(dpi=300)
-plt.errorbar(df.columns.values, avgs, yerr=stds, marker="x", capsize=2, 
-             linewidth=0.5, elinewidth=0.5, color="black", ecolor="red")
+plt.errorbar(
+    df.columns.values,
+    avgs,
+    yerr=stds,
+    marker="x",
+    capsize=2,
+    linewidth=0.5,
+    elinewidth=0.5,
+    color="black",
+    ecolor="red",
+)
 plt.xlabel("Dimensions")
 plt.ylabel("Residual")
 plt.savefig("plot")
